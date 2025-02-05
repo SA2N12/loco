a70e34201eb2fbe3c9ec8569385a0c63eb0ef920
SA2N12 <lecorrejules10@gmail.com>
2025-02-05 14:13:27 +0100

    Initial commit

---
+++ src/main/java/fr/eni/loco/loco/LocoApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.loco.loco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/loco/loco/LocoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.loco.loco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
